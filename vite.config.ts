f522e2864a3e0e28d5ededffc43e3f4314f10b79
simon-andersen-nellemann <sian@nellemann.dk>
2026-01-07 10:05:36 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  define: {
+    'process.env': {}
+  },
   build: {
     outDir: 'public',
     rollupOptions: {
